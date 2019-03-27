10288bdb0f1cc47f8d30a4a585f01b3820401723
Kraramel <asri.med.amine@gmail.com>
2019-03-27 21:25:37 +0100

    updating entries

--- server.js
+++ server.js
@@ -75,31 +75,27 @@ app.get('/profile/:id', (req, res) => {
     db.select('*').from('users').where({
         id: id
     })
-    .then(user => {
-        if (user.length) {
-            res.json(user[0]);
-        }
-        else {
-            res.status(400).json('Not found')
-        }
-    })
-    .catch(err => res.status(400).json('error getting the user'))
-    })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0]);
+            }
+            else {
+                res.status(400).json('Not found')
+            }
+        })
+        .catch(err => res.status(400).json('error getting the user'))
+})
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
     let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
+    db('users').where('id', '=', id)
+    .increment('entries', 1)        
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
     })
-
-    if (!found) {
-        res.status(400).json('Not Found');
-    }
+    .catch( err => res.status(400).json('unable to get entries'))
 })
 
 app.listen(3999, () => {
