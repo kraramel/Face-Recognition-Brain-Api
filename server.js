01e0d5998ead63a67822dfa866b0c19368a0db2b
Kraramel <asri.med.amine@gmail.com>
2019-03-27 21:11:25 +0100

    Getting user profiles

--- server.js
+++ server.js
@@ -72,17 +72,19 @@ app.post('/register', (req, res) => {
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
     let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            res.json(user);
+    db.select('*').from('users').where({
+        id: id
+    })
+    .then(user => {
+        if (user.length) {
+            res.json(user[0]);
+        }
+        else {
+            res.status(400).json('Not found')
         }
     })
-
-    if (!found) {
-        res.status(400).json('Not Found');
-    }
-})
+    .catch(err => res.status(400).json('error getting the user'))
+    })
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
@@ -101,7 +103,7 @@ app.put('/image', (req, res) => {
 })
 
 app.listen(3999, () => {
-    console.log('app is running on port 3000');
+    console.log('app is running on port 3999');
 })
 
 
