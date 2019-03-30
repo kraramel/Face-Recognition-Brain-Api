0fe827e6b73853dab542da86e0878bc64d3724d0
Kraramel <asri.med.amine@gmail.com>
2019-03-30 14:22:49 +0100

    adding the validation code

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,9 @@
 const saltRounds = 10;
 const handleRegister = (req, res, db, bcrypt) => {
     const { email, name, password } = req.body;
+    if (!email || !password || !name) {
+        return res.status(400).json('incorrect form submission')
+    }
     const hash = bcrypt.hashSync(password, saltRounds);
 
     db.transaction(trx => {
